106c1cab44a464414593998eedd771b724a0aff6
ashkanzabihi <zabihiashkan71@gmail.com>
2020-06-23 11:00:56 +0430

    change templates

--- ashkansite/settings.py
+++ ashkansite/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+)
